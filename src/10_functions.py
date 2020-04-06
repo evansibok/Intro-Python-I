1841716b7c0234697e35326258f20f864d4f001e
Evans Ibok <evansibok@gmail.com>
2020-04-06 18:18:44 +0100

    complete exercise 10

--- src/10_functions.py
+++ src/10_functions.py
@@ -2,11 +2,23 @@
 
 # YOUR CODE HERE
 
+
+def is_even(num):
+    if num % 2 == 0:
+        print(True)
+    else:
+        print(False)
+
+
 # Read a number from the keyboard
 num = input("Enter a number: ")
 num = int(num)
+is_even(num)
 
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 
 # YOUR CODE HERE
-
+if num % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
