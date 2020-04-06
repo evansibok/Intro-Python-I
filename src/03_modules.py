d834a936011374ae0baeaa6ed12d60bf3f2908c6
Evans Ibok <evansibok@gmail.com>
2020-04-06 14:31:06 +0100

    add some answers to exercise 03

--- src/03_modules.py
+++ src/03_modules.py
@@ -1,24 +1,28 @@
 """
 In this exercise, you'll be playing around with the sys module,
 which allows you to access many system specific variables and
-methods, and the os module, which gives you access to lower-
-level operating system functionality.
+methods, and the os module, which gives you access to lower-level operating system functionality.
 """
 
+import os
 import sys
 # See docs for the sys module: https://docs.python.org/3.7/library/sys.html
 
 # Print out the command line arguments in sys.argv, one per line:
 # YOUR CODE HERE
+# print("sys", dir(sys))
+# print("sys.argv", dir(sys.argv))
+# for m in sys.argv:
+#     print("Printed Argv", m)
 
 # Print out the OS platform you're using:
 # YOUR CODE HERE
+print("OS Name >>", os.name)
 
 # Print out the version of Python you're using:
 # YOUR CODE HERE
+print("Python Version", sys.version)
 
-
-import os
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
 
 # Print the current process ID
